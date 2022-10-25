117eae9efbf67215aee94d45977f4215cc57faa9
axyn45 <axyn45@outlook.com>
2022-10-25 16:21:24 +0000

    Fixed not-getting-author-avatar issue in footer

--- layouts/authorprofile.php
+++ layouts/authorprofile.php
@@ -7,7 +7,7 @@ if (iro_opt('author_profile') == '1') {
 ?>
 	<section class="author-profile">
 		<div class="info" itemprop="author" itemscope="" itemtype="http://schema.org/Person">
-			<a href="<?= esc_url(get_author_posts_url(get_the_author_meta('ID'))); ?>" class="profile gravatar"><img src="<?php echo get_avatar_profile_url(); ?>" itemprop="image" alt="<?php the_author(); ?>" height="70" width="70"></a>
+			<a href="<?= esc_url(get_author_posts_url(get_the_author_meta('ID'))); ?>" class="profile gravatar"><img src="<?php echo get_avatar_url(get_the_author_meta( 'ID' )); ?>" itemprop="image" alt="<?php the_author(); ?>" height="70" width="70"></a>
 			<div class="meta">
 				<span class="title"><?php esc_html_e('Author', 'akina'); ?></span>	
 				<h3 itemprop="name">
